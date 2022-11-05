aa3eac0006e11b2d370a664b4ade0f0820404d19
masudrana5 <abdullainfo30@gmail.com>
2022-11-05 21:08:50 +0600

    vercel config and build package add

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
       
       app.post('/jwt', (req, res) => {
          const user = req.body;
-         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5' })
+         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
          res.send({token});
       })
 
@@ -57,9 +57,8 @@ async function run() {
       // orders api
       app.get('/orders', verifyJWT, async (req, res) => {
          const decoded = req.decoded;
-         console.log(decoded);
          if (decoded.email !== req.query.email) {
-            res.status(403).send({message: 'unauthorized access'})
+            return res.status(403).send({message: 'forbidden access'})
          }
          let query = {}
          if (req.query.email) {
@@ -72,13 +71,13 @@ async function run() {
          res.send(orders)
       });
 
-      app.post('/orders', async (req, res) => {
+      app.post('/orders',verifyJWT, async (req, res) => {
          const order = req.body;
          const result = await orderCollection.insertOne(order);
          res.send(result);
       });
 
-      app.patch('/orders/:id', async (req, res) => {
+      app.patch('/orders/:id', verifyJWT, async (req, res) => {
          const id = req.params.id;
          const status = req.body.status;
          const query = { _id: ObjectId(id) };
@@ -91,7 +90,7 @@ async function run() {
          res.send(result);
       });
 
-      app.delete('/orders/:id', async (req, res) => {
+      app.delete('/orders/:id', verifyJWT, async (req, res) => {
          const id = req.params.id;
          const query = { _id: ObjectId(id) }
          const result = await orderCollection.deleteOne(query);
